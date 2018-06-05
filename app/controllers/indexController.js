da36c83f102d8f953b437ca60378019112c77600
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2018-06-05 11:05:33 +0200

    added gulp-pug to compile pug files

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -4,7 +4,5 @@ let path = require('path');
 
 exports.index = function(req, res) {
 
-    console.log("OOOOOOOOOOOOOOOOOOOOOOS");
-    console.log(__dirname);
     res.sendFile('index.html', { root: './static' });
 };
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const concatCss = require('gulp-concat-css');
+const pug = require('gulp-pug');
 
 const browserify = require('browserify');
 const babelify = require('babelify');
@@ -16,48 +17,45 @@ const source = require("vinyl-source-stream");
 const destination = './static';
 const es6 = true;
 
-gulp
-    .task('default', ['main'], (callback) => {
+gulp.task('default', ['main'], (callback) => {
         callback();
-    })
-    .task('main', ['html', 'css', 'js', 'vendors.css', 'vendors.js', 'fonts'], (callback) => {
-        callback();
-    })
-    .task('html', () => {
-        gulp.src('views/**/*.html')
-            .pipe(gulp.dest(destination));
-    })
-    .task('css', () => {
-        gulp.src('views/**/*.css')
-            .pipe(cleanCSS())
-            .pipe(concatCss('app.min.css'))
-            .pipe(gulp.dest(destination));
-    })
-    .task('js', () => {
-        browserify({ entries: `./views/js/${es6 ? 'es6-version' : 'es5-version'}/joliform.js` })
-            .transform(babelify.configure({ presets: ['es2015'] }))
-            .bundle()
-            .pipe(source('app.min.js'))
-            .pipe(gulp.dest(destination));
 
-        gulp.src('./views/js/init.js')
-            .pipe(concat('init.js'))
-            .pipe(gulp.dest(destination));
-    })
-    .task('fonts', () => {
-        gulp.src('views/fonts/*.*')
-            .pipe(gulp.dest(destination + '/fonts'));
-    })
-    .task('vendors.js', () => {
-        gulp.src(mainBowerFiles('**/*.js'))
-            .pipe(concat('vendors.min.js'))
-            .pipe(gulp.dest(destination + '/lib'));
-    })
-    .task('vendors.css', () => {
-        gulp.src(mainBowerFiles('**/*.css'))
-            .pipe(concat('vendors.min.css'))
-            .pipe(gulp.dest(destination + '/lib'));
-    })
-    .task('watch', () => {
+}).task('main', ['pug', 'css', 'js', 'vendors.css', 'vendors.js', 'fonts'], (callback) => {
+        callback();
+
+}).task('pug', () => {
+    gulp.src('views/index.pug').pipe(pug()).pipe(gulp.dest(destination));
+
+}).task('css', () => {
+    gulp.src('views/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(concatCss('app.min.css'))
+        .pipe(gulp.dest(destination));
+
+}).task('js', () => {
+    browserify({ entries: `./views/js/${es6 ? 'es6-version' : 'es5-version'}/joliform.js` })
+        .transform(babelify.configure({ presets: ['es2015'] }))
+        .bundle()
+        .pipe(source('app.min.js'))
+        .pipe(gulp.dest(destination));
+
+    gulp.src('./views/js/init.js')
+        .pipe(concat('init.js'))
+        .pipe(gulp.dest(destination));
+
+}).task('fonts', () => {
+    gulp.src('views/fonts/*.*').pipe(gulp.dest(destination + '/fonts'));
+
+}).task('vendors.js', () => {
+    gulp.src(mainBowerFiles('**/*.js'))
+        .pipe(concat('vendors.min.js'))
+        .pipe(gulp.dest(destination + '/lib'));
+
+}).task('vendors.css', () => {
+    gulp.src(mainBowerFiles('**/*.css'))
+        .pipe(concat('vendors.min.css'))
+        .pipe(gulp.dest(destination + '/lib'));
+
+}).task('watch', () => {
         gulp.watch('views/**/*.{html,css,js}', ['html', 'css', 'js']);
-    });
+});
